f29efa87fb0f4248537626fe3c8325c1184ccd86
David Ryan <11883046+davidryan59@users.noreply.github.com>
2024-07-24 14:30:02 +0700

    Add Fraction class

---
+++ src/Fraction.py
@@ -0,0 +1,26 @@
+from src.functions import gcd
+
+
+class Fraction:
+
+    # TODO: add some different methods of initialisation
+    def __init__(self, num: int, denom: int) -> None:
+        if type(num) != int or type(denom) != int:
+            raise Exception("Cannot initialise Fraction on non-integer numerator or denominator")
+
+        if denom < 1:
+            raise Exception("Cannot initialise Fraction on denominator < 1")
+
+        _gcd = gcd(num, denom)
+        self.num = int(num / _gcd)  # int shouldn't change the result, but does change the type from float to int
+        self.denom = int(denom / _gcd)
+
+
+    def __repr__(self):
+        if self.denom == 1:
+            return f"{self.num}"
+        else:
+            return f"{self.num}/{self.denom}"
+
+
+    # TODO: add __mul__, __truediv__, etc
